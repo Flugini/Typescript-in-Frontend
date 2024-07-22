b02d04c8acd78f7613993ba4e0e41d9fbb8fdf1e
Zana Flugini <zanaflugu@gmail.com>
2024-07-22 15:01:26 +0200

    Add interface Taskinfo and function displayTask

--- src/task.ts
+++ src/task.ts
@@ -14,3 +14,18 @@ export default Task;
 
 const task1 = new Task("Complete Assignment", false);
 console.log(task1)
+
+
+interface Taskinfo {
+    title: string, 
+    completed: boolean
+}
+
+function displayTask(task: Taskinfo): void {
+
+}
+
+//Example usage
+const task1Info: Taskinfo = {
+    title: "Complete assignment", completed: false
+}
